84d4ef71188590b4656eec04b3678d50b5071c71
user <user@DESKTOP-3RNLDO3>
2020-08-10 16:19:24 +0530

    URL updates

--- AllianzTeamChallenge_Batch10/src/main/java/com/allianz/teamchallenge/web/CustomerController.java
+++ AllianzTeamChallenge_Batch10/src/main/java/com/allianz/teamchallenge/web/CustomerController.java
@@ -20,35 +20,31 @@ import com.allianz.teamchallenge.model.DistrictEntity;
 import com.allianz.teamchallenge.service.CustomerService;
 
 @RestController
-@RequestMapping("/customer")
+@RequestMapping("/v1/customer/sensordata")
 public class CustomerController {
 
 	@Autowired
 	CustomerService customerService;
 	
-	//Get all data
 	@GetMapping(value = "/", produces = { "application/xml", "text/xml" }, consumes = MediaType.ALL_VALUE)
     public ResponseEntity<List<CustomerEntity>> getAllSensorData() throws RecordNotFoundException {
 		List<CustomerEntity> customerEntities = customerService.getAllSensorData();
         return new ResponseEntity<List<CustomerEntity>>(customerEntities, new HttpHeaders(), HttpStatus.OK);
     }
 	
-	//Get CO2 emission reading against a customer
 	@GetMapping(value = "/{id}", produces = { "application/xml", "text/xml" }, consumes = MediaType.ALL_VALUE)
 	public ResponseEntity<CustomerEntity> getSensorDataByCustomerId(@PathVariable("id") Integer id) throws RecordNotFoundException {
 		CustomerEntity customerEntity = customerService.getSensorDataByCustomerId(id);
         return new ResponseEntity<CustomerEntity>(customerEntity, new HttpHeaders(), HttpStatus.OK);
     }
 	
-	//Get CO2 emission reading against a city
-	@GetMapping(value = "{customerid}/city/{cityid}", produces = { "application/xml", "text/xml" }, consumes = MediaType.ALL_VALUE)
+	@GetMapping(value = "/{customerid}/city/{cityid}", produces = { "application/xml", "text/xml" }, consumes = MediaType.ALL_VALUE)
     public ResponseEntity<CityEntity> getSensorDataByCityId(@PathVariable("customerid") Integer customer_id,
     		@PathVariable("cityid") Integer city_id) throws RecordNotFoundException {
 		CityEntity cityEntity = customerService.getSensorDataByCityId(customer_id, city_id);
         return new ResponseEntity<CityEntity>(cityEntity, new HttpHeaders(), HttpStatus.OK);
     }
 	
-	//Get CO2 emission reading against a district
 	@GetMapping(value = "/{customerid}/city/{cityid}/district/{districtid}", produces = { "application/xml", "text/xml" }, consumes = MediaType.ALL_VALUE) 
 	public ResponseEntity<DistrictEntity> getSensorDataByDistrictId(@PathVariable("customerid") Integer customer_id, 
 			@PathVariable("cityid") Integer city_id, @PathVariable("districtid") Integer district_id) throws RecordNotFoundException { 
@@ -56,8 +52,7 @@ public class CustomerController {
 		return new ResponseEntity<DistrictEntity>(districtEntity, new HttpHeaders(), HttpStatus.OK); 
 	}
 	
-	//Save customer, city, district and sensor data to CB
-	@PostMapping()
+	@PostMapping(value = "/create", produces = { "application/xml", "text/xml" }, consumes = { "application/xml", "text/xml" })
     public ResponseEntity<CustomerEntity> createCustomers(@RequestBody CustomerEntity customer)
                                                     throws RecordNotFoundException {
 		CustomerEntity customerData = customerService.createCustomers(customer);
--- AllianzTeamChallenge_Batch10/src/main/resources/data.sql
+++ AllianzTeamChallenge_Batch10/src/main/resources/data.sql
@@ -1,10 +1,4 @@
-INSERT INTO 
-	TBL_EMPLOYEES (first_name, last_name, email) 
-VALUES
-  	('Lokesh', 'Gupta', 'howtodoinjava@gmail.com'),
-  	('John', 'Doe', 'xyz@email.com');
-
-  	
+ 	
 INSERT INTO 
 	TBL_CUSTOMERS (first_name, last_name, email, city_id) 
 VALUES
